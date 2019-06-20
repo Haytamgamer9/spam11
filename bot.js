5a17ac114eabd0326f36892e38cb447b06733b5c
Haytamgamer9 <44749438+Haytamgamer9@users.noreply.github.com>
2019-06-20 13:27:57 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "!";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "590908301252755476"; // ايدي السررفر
+var channel = "590908301252755479";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send(' سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحانhaytam الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,')
+    },305);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);
